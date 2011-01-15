55b9046a250bf605660799e578389801a03cbaea
mdeimel <mdeimel@gmail.com>
2011-01-14 22:17:58 -0600

    committing everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BDR::Application.config.secret_token = 'daa9c6381871619b878accb776ecb590175929d060d175e764597e5ccd3a79681d960855eae59b1371e07dffb6c1f2238d1ac6324bef00ed048f78d0459918ee'
