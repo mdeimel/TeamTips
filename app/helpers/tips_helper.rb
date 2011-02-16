d6a2cb277366ae532b79fddd59b66ce3d12956a0
mdeimel <mdeimel@gmail.com>
2011-02-15 20:36:25 -0600

    Fixing results listing to not display duplicate lines

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -1,2 +1,18 @@
 module TipsHelper
+  def highlight_body body, search_split
+    results = Array.new
+    search_vals = ""
+    search_split.each do |split| search_vals+=split+"|" end
+    search_vals.chop! # Remove extra pipe character
+    counter = 0
+    body.each_line do |line|
+      break if counter >= 5
+      replaced_line = line.gsub(/#{search_vals}/i) { |val| "<b>#{val}</b>" }
+      if replaced_line != line
+        results.push replaced_line
+        counter+=1
+      end
+    end
+    results
+  end
 end
